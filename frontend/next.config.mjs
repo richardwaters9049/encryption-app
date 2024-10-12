ed21b23e94da23dff02b90cafc1c6ccf79d3decb
richardwaters9049 <119029825+richardwaters9049@users.noreply.github.com>
2024-10-12 23:21:57 +0100

    nextjs change

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true, // Enables React strict mode
+    experimental: {
+        appDir: true, // If you are using the new app directory feature
+    },
+    env: {
+        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL, // Make the API URL available to the client-side
+    },
+};
 
 export default nextConfig;
